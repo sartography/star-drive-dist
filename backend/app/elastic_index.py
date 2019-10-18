a675694d501af488d6470f94de1f382903a7e84b
Dan Funk <daniel.h.funk@gmail.com>
2019-10-18 10:42:15 -0400

    updates to the forms and events.

--- backend/app/elastic_index.py
+++ backend/app/elastic_index.py
@@ -2,7 +2,7 @@ import datetime
 
 from elasticsearch import RequestError
 from elasticsearch_dsl import Date, Keyword, Text, Index, analyzer, Integer, tokenizer, Document, Double, GeoPoint, \
-    Search, A, Boolean
+    Search, A, Boolean, analysis
 from elasticsearch_dsl.connections import connections
 import logging
 
@@ -17,6 +17,11 @@ autocomplete_search = analyzer('autocomplete_search',
                                tokenizer=tokenizer('lowercase')
                                )
 
+english_stem_filter = analysis.token_filter('my_english_filter', name="minimal_english", type="stemmer")
+stem_analyzer = analyzer('stem_analyzer',
+                        tokenizer=tokenizer('standard'),
+                        filter=['lowercase', english_stem_filter])
+
 
 # Star Documents are ElasticSearch documents and can be used to index an Event,
 # Location, Resource, or Study
@@ -24,11 +29,11 @@ class StarDocument(Document):
     type = Keyword()
     label = Keyword()
     id = Integer()
-    title = Text(analyzer=autocomplete, search_analyzer=autocomplete_search)
+    title = Text()
     date = Date()
     last_updated = Date()
-    content = Text(analyzer=autocomplete, search_analyzer=autocomplete_search)
-    description = Text(analyzer=autocomplete, search_analyzer=autocomplete_search)
+    content = Text(analyzer=stem_analyzer)
+    description = Text()
     organization = Keyword()
     website = Keyword()
     location = Keyword()
@@ -131,19 +136,22 @@ class ElasticIndex:
 
         if hasattr(document, 'date'):
             doc.date = document.date
+        if hasattr(document, 'website'):
+            doc.website = document.website
+        if hasattr(document, 'status') and document.status is not None:
+            doc.status = document.status.value
 
         doc.meta.id = self._get_id(document)
-
-        if document.__tablename__ is not 'study':
-            doc.website = document.website
-        elif document.status is not None:
-            doc.status = document.status.value
 
         if document.organization is not None:
             doc.organization = document.organization.name
 
         for cat in document.categories:
             doc.category.extend(cat.category.all_search_paths())
+
+        if document.__tablename__ is 'study':
+            doc.title = document.short_title
+            doc.description = document.short_description
 
         if (doc.type in ['location', 'event']) and None not in (latitude, longitude):
             doc.latitude = latitude
@@ -173,13 +181,12 @@ class ElasticIndex:
         if not search.words:
             query = MatchAll()
         else:
-            query = MultiMatch(query=search.words, fields=['title^10', 'content^5', 'description^5', 'location^3',
-                                                           'category^2', 'organization', 'website'])
+            query = MultiMatch(query=search.words, fields=['content'])
 
         elastic_search = Search(index=self.index_name)\
             .doc_type(StarDocument)\
             .query(query)\
-            .highlight('content', fragment_size=50)
+            .highlight('content', type='unified', fragment_size=50)
 
         elastic_search = elastic_search[search.start:search.start + search.size]
 
